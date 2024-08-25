9e3355854c5af55109fbaf0e338fa5a2817793f5
Michi Koodaaja <p31516@edu.sasky.fi>
2024-08-25 20:33:53 +0300

    silausta

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e36c76dbc693827ba3d664d202963041a35c704b',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e36c76dbc693827ba3d664d202963041a35c704b',
+            'dev_requirement' => false,
+        ),
+        'league/plates' => array(
+            'pretty_version' => 'v3.5.0',
+            'version' => '3.5.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../league/plates',
+            'aliases' => array(),
+            'reference' => 'a6a3238e46c6e19af7318fdc36bfbe49b0620231',
+            'dev_requirement' => false,
+        ),
+    ),
+);
